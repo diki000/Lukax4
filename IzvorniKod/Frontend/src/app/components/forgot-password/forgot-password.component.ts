191edf110239d300ae6980688c851408bbd1936a
fjakaZ <lz54269@fer.hr>
2023-11-02 01:22:42 +0100

    popravljen dizajn forgot-password komponente

--- IzvorniKod/Frontend/src/app/components/forgot-password/forgot-password.component.ts
+++ IzvorniKod/Frontend/src/app/components/forgot-password/forgot-password.component.ts
@@ -15,7 +15,14 @@ export class ForgotPasswordComponent {
   sentCode: string = '';
   newPassword: string = '';
   confirmPassword: string = '';
+
+  showEnterEmailForm: boolean = true;
+
+  showVerificationCodeForm: boolean = false;
+  showWrongCodeMessage: boolean = false;
+
   showPasswordResetForm: boolean = false;
+  showWrongPasswordMessage: boolean = false;
 
   focusNext(nextDigit: number) {
     const nextInput = document.getElementById(`digit${nextDigit}`) as HTMLInputElement;
@@ -24,22 +31,27 @@ export class ForgotPasswordComponent {
     }
   }
 
-  onReset() {
+  onSendCode() {
     this.sentCode = '' //ovo je u slucaju da se vise puta klikne na reset(da se ne dodaje 5 znamenki na jos 5 znamenki na jos...)
     console.log(`Password reset requested for email: ${this.email}`);
     for (let i = 0; i < 5; i++){
         this.sentCode += Math.floor(Math.random() * 10).toString();
     }
     console.log(`Code sent on email: ${this.sentCode}`);
+    window.alert(this.sentCode)
+    this.showEnterEmailForm = false;
+    this.showVerificationCodeForm = true;
   }
 
-  onSubmit() {
+  onSubmitCode() {
     const code = this.digit1 + this.digit2 + this.digit3 + this.digit4 + this.digit5;
     if (code == this.sentCode) {
         console.log("same code")
+        this.showVerificationCodeForm = false;
         this.showPasswordResetForm = true;
     } else {
         console.log("wrong code")
+        this.showWrongCodeMessage = true;
     }
 
   }
@@ -47,8 +59,10 @@ export class ForgotPasswordComponent {
   onNewPassword() {
     if (this.newPassword === this.confirmPassword) {
       console.log('Password reset successful');
+      this.showWrongPasswordMessage = false;
     } else {
       console.log('Passwords do not match');
+      this.showWrongPasswordMessage = true;
     }
   }
 }
