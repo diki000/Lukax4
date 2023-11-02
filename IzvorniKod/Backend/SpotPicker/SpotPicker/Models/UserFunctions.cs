56fd885743cfeb871f27d5615577a55ee8eaf45e
Dominik Poljak <dominik.poljak1@gmail.com>
2023-11-02 18:59:45 +0100

    Added email sending service and email validation

--- IzvorniKod/Backend/SpotPicker/SpotPicker/Models/UserFunctions.cs
+++ IzvorniKod/Backend/SpotPicker/SpotPicker/Models/UserFunctions.cs
@@ -1,13 +1,17 @@
 ﻿using SpotPicker.EFCore;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SpotPicker.Models
 {
     public class UserFunctions
     {
         private readonly _EFCore _db;
-        public UserFunctions(_EFCore database)
+        private readonly IConfiguration _config;
+        public UserFunctions(_EFCore database, IConfiguration config)
         {
             _db = database;
+            _config = config;
         }
 
         public void register(UserModel user)
@@ -21,6 +25,25 @@ namespace SpotPicker.Models
 
             _db.User.Add(newUser);
             _db.SaveChanges();
+        }
+
+        public bool verifyEmail(int id, string tokenFromUrl)
+        {
+            string hashKey = _config["SecredHashKey"]!;
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] hashedBytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(id.ToString() + hashKey));
+                string recreatedToken = BitConverter.ToString(hashedBytes).Replace("-", "").ToLower();
+
+                if (recreatedToken == tokenFromUrl)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
     }
 }
