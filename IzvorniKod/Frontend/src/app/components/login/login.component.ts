a6cf646a400279bd2ea75a2498d8f9d6e91c5793
mirta <mirta.posnjak@gmail.com>
2023-11-05 17:15:10 +0100

    prva verzija login

--- IzvorniKod/Frontend/src/app/components/login/login.component.ts
+++ IzvorniKod/Frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
-}
+  userdata: any;
+
+  constructor(private builder: FormBuilder, private service: AuthService,
+    private router: Router) {
+
+  }
+
+  loginForm = this.builder.group({
+    username: this.builder.control('', Validators.required),
+    password: this.builder.control('', Validators.required)
+  })
+
+  proceedLogin() {
+    if (this.loginForm.valid) {
+      this.service.Getbycode(this.loginForm.value.username).subscribe(res => {
+        this.userdata = res;
+        console.log(this.userdata);
+      });
+    } else {
+      console.log("Error invalid form");
+    }
+  }
