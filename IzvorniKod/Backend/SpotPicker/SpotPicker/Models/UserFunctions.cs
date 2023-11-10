1cc8cce1e3d71b3b5f78954590ec810f5c9bf56f
diki000 <luka.diktic@gmail.com>
2023-11-10 21:25:03 +0100

    added login to userFunctions

--- IzvorniKod/Backend/SpotPicker/SpotPicker/Models/UserFunctions.cs
+++ IzvorniKod/Backend/SpotPicker/SpotPicker/Models/UserFunctions.cs
@@ -50,7 +50,98 @@ namespace SpotPicker.Models
 
             return remainder == 1;
         }
+        public UserModel login(string username, string password)
+        {
+            // Find the user in the database by username
+            User existingUser = _db.User.FirstOrDefault(user => user.Username == username);
 
+            // If the user does not exist, throw an exception or handle the situation accordingly
+            if (existingUser == null)
+            {
+                Console.Write("NEPOSTOJI OVAJ USER \n\n\n");
+
+
+                //return 400; //vrati 400, user not found
+                var ex = new Exception();
+                ex.Data["Kod"] = 400;
+                throw ex;
+                //throw new Exception("User not found. Status code: 400");
+            }
+
+
+            if (existingUser != null)
+            {
+                Console.Write("\n\nPOSTOJI OVAJ USER\n\n");
+                Console.Write(existingUser.Username);
+                Console.Write("\n\n");
+                Console.Write(existingUser.Password);
+                Console.Write("\n\n");
+
+            }
+
+            // User-entered password
+            string enteredPassword = password; // uzmi lozinku unesenu od strane usera na frontu
+            string storedPasswordHash = existingUser.Password; // checkiraj lozinku iz baze
+            // Verify the entered password
+            bool isPasswordValid = BCrypt.Net.BCrypt.Verify(enteredPassword, storedPasswordHash); // odradi hashing
+
+            if (isPasswordValid)
+            {
+                Console.Write("VALJA LOZINKA NAKON HASHINGA \n\n");
+            }
+            else
+            {
+                Console.Write("NEVALJA LOZINKA NAKON HASHINGA \n\n");
+            }
+
+            UserModel korisnik = new UserModel();
+
+            if (existingUser != null)
+            {
+                korisnik.Username = existingUser.Username;
+                korisnik.Password = existingUser.Password;
+                korisnik.Name = existingUser.Name;
+                korisnik.Surname = existingUser.Surname;
+                korisnik.IBAN = existingUser.IBAN;
+                korisnik.Email = existingUser.Email;
+                korisnik.IsEmailConfirmed = existingUser.IsEmailConfirmed;
+                korisnik.RoleID = existingUser.RoleID;
+            }
+
+            //povezi ga s manager tablicom
+            Manager managerCheck = _db.Manager.FirstOrDefault(m => m.UserId == existingUser.Id);
+
+            if (isPasswordValid)
+            {
+                if (existingUser.IsEmailConfirmed == false)
+                {
+                    // throw new Exception(string.Format("{0} - {1}", statusMessage, statusCode));
+                    //throw new Exception("Mail not confirmed. Status code: 401"); // nije potvrdjen mail
+                    var ex = new Exception();
+                    ex.Data["Kod"] = 401;
+                    throw ex;
+
+                }
+
+                // ako je user vlasnik parkinga, provjeri je li potvrdjen od admina, ako nije returnaj error
+                if (existingUser.RoleID == 2 && !managerCheck.ConfirmedByAdmin)
+                {
+                    var ex = new Exception();
+                    ex.Data["Kod"] = 403;
+                    throw ex;
+                    //throw new Exception("Owner not confirmed by admin. Status code: 403"); // vlasnik parkinga nije potvrdjen od admina
+                }
+
+                return korisnik; // AKO SVE VALJA, VRATI USERA NA FRONT
+            }
+            else
+            {
+                //throw new Exception("Incorrect password. Status code: 402"); //lozinka nevalja
+                var ex = new Exception();
+                ex.Data["Kod"] = 402;
+                throw ex;
+            }
+        }
         public void register(UserModel user)
         {
             bool usernameAvailable = !_db.User.Any(u => u.Username == user.Username);
