74db5144fbd95ade7655638258f43574740f67e8
Filip Sučić <fs54014@fer.hr>
2023-11-13 18:19:42 +0100

    Dodan admin kontroler, funkcije za admin sucelje

---
+++ IzvorniKod/Backend/SpotPicker/SpotPicker/Controllers/AdminController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SpotPicker.EFCore;
+using SpotPicker.Models;
+using SpotPicker.Services;
+using System.Text.Json;
+using System.Text.Json.Nodes;
+using System.Text.Json.Serialization;
+
+namespace SpotPicker.Controllers
+{
+    public class AdminController : Controller
+    {
+        private _EFCore _db;
+        private AdminFunctions _adminFunctions;
+        private IConfiguration _config;
+        public AdminController(_EFCore dataContext, IConfiguration con)
+        {
+            _db = dataContext;
+            _config = con;
+            _adminFunctions = new AdminFunctions(dataContext, _config);
+        }
+
+        [HttpGet]
+        [Route("api/[controller]/GetUnacceptedManagers")]
+        public IActionResult GetUnacceptedManagers()
+        {
+            var t = _adminFunctions.GetUnacceptedManagers();
+            return Ok(t);
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/AcceptManager")]
+        public IActionResult AcceptManager(string username)
+        {
+            _adminFunctions.AcceptManager(username);
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("api/[controller]/DeclineManager")]
+        public IActionResult DeclineManager(string username)
+        {
+            _adminFunctions.DeclineManager(username);
+            return Ok();
+        }
+    }
+}
