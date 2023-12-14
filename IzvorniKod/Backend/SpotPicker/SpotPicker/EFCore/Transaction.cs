59b6bbe6876d9e93b0d82ac3f72090daadbcc9e9
Babbage <lukababicoib@gmail.com>
2023-12-14 17:11:22 +0100

    add database definitions

---
+++ IzvorniKod/Backend/SpotPicker/SpotPicker/EFCore/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using SpotPicker.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotPicker.EFCore
+{
+    [Table("Transaction")]
+    public class Transaction
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public int UserID { get; set; }
+
+        [ForeignKey("User")]
+        public virtual User User { get; set; }
+
+        public int Type { get; set; }
+
+        public float Amount { get; set; }
+    }
+}
---
+++ IzvorniKod/Backend/SpotPicker/SpotPicker/EFCore/Wallet.cs
@@ -0,0 +1,20 @@
+﻿using SpotPicker.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SpotPicker.EFCore
+{
+    [Table("Wallet")]
+    public class Wallet
+    {
+        [Key]
+        public int WalletID { get; set; }
+
+        public int UserID { get; set; }
+
+        [ForeignKey("User")]
+        public virtual User User { get; set; }
+
+        public float Balance { get; set; }
+    }
+}
