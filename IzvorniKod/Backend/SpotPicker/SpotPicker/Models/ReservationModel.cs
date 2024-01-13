618b8b1225428cd2923cc62c227d7d10973c30c7
Babbage <lukababicoib@gmail.com>
2024-01-13 22:30:27 +0100

    setup database connections for reservations

---
+++ IzvorniKod/Backend/SpotPicker/SpotPicker/Models/ReservationModel.cs
@@ -0,0 +1,16 @@
+﻿using SpotPicker.EFCore;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace SpotPicker.Models
+{
+    public class ReservationModel
+    {
+        public int ReservationID { get; set; }
+        public int UserID { get; set; }
+        public int ParkingSpaceID { get; set; }
+        public DateTime ReservationDate { get; set; }
+        public DateTime ReservationDuration { get; set; }
+        public bool IsRepeating { get; set; }
+    }
+}
