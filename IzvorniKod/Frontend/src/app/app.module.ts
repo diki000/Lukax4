514544885632c7c5ad7aeebdcf660722320121d1
Dominik Poljak <dominik.poljak1@gmail.com>
2024-01-15 23:16:41 +0100

    fixes

--- IzvorniKod/Frontend/src/app/app.module.ts
+++ IzvorniKod/Frontend/src/app/app.module.ts
@@ -41,8 +41,6 @@ import { RadioButtonModule } from 'primeng/radiobutton';
     SidebarComponent,
     UnregisteredMapComponent,
     ReservationComponent,
-    
-,
     StatisticsComponent
   ],
   imports: [
